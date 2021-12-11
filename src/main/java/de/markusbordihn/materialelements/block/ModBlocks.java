00a9d39deb142d72e1c3165b7503a57bc2ed44d8
Markus Bordihn <Markus@Bordihn.de>
2021-12-11 23:01:30 +0100

    Updated .gitignore file.

--- src/main/java/de/markusbordihn/materialelements/block/ModBlocks.java
+++ src/main/java/de/markusbordihn/materialelements/block/ModBlocks.java
@@ -24,6 +24,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.OreBlock;
 import net.minecraft.block.material.Material;
+
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
